f9cb823da8cb6fd486f6d56d1110312a908c1c85
venkat <subash.venkat007@gmail.com>
2021-07-08 15:48:43 +0530

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,4 @@
+export default {
+  vite: false
+  // modules: ["./codesandbox"]
+};
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <section>Reproduction</section>
+</template>
+
+<script>
+export default {};
+</script>
